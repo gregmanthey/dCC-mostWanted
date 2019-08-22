dbeae69f806e2489a2a0ae2e538cf2b35e059052
gregmanthey <gmanthey03@gmail.com>
2019-08-22 09:46:43 -0500

    collaboration test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 /*
 Build all of your functions for displaying and gathering information below (GUI).
 */
-
+// random comment
 // app is the function called to start the entire application
 function app(people){
   let searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
